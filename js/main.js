20142ccfb530037fe009f527951ea8ccf46a043e
Elliot Hwang <elliothwang@MacBook-Pro-2.local>
2021-03-09 12:33:15 -0800

    fix boards, add event listeners & stub up some functions

--- js/main.js
+++ js/main.js
@@ -9,34 +9,6 @@ let colorLookup = {
     "2": "pink"
 }
 
-let playerBoardArr = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 9
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 8
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 7
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 6
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 5
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 4
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 3
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 2
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 1
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 0
-//   a  b  c  d  e  f  g  h  i  j
-];
-
-let pcBoardArr = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 9
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 8
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 7
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 6
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 5
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 4
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 3
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 2
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 1
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 0
-//   a  b  c  d  e  f  g  h  i  j
-];
-
 let boatsArray = [];
 
 
@@ -50,16 +22,61 @@ pcBoardEl = document.querySelector("#pc-board");
 msgEl = document.querySelector("#msg");
 playerStatusEl = document.querySelector("#player-boats-status")
 playerBoardEl = document.querySelector("#player-board");
+rotateButton = document.querySelector("#rotate");
 replayButton = document.querySelector("#reset");
+
+// ===== event listeners ===== //
+playerBoardEl.addEventListener("click", handleClick);
+rotateButton.addEventListener("click", rotate);
+replayButton.addEventListener("click", init);
+
 
 // ===== functions ===== //
 
 function init () {
+console.log("resetting!")
+    winner = null;
+    turn = 1;
 
+    playerBoard = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 9
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 8
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 7
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 6
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 5
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 4
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 3
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 2
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 0
+    //   a  b  c  d  e  f  g  h  i  j
+    ];
+
+    pcBoard = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 9
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 8
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 7
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 6
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 5
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 4
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 3
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 2
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // row 0
+    //   a  b  c  d  e  f  g  h  i  j
+    ];
+    }
+
+function handleClick (evt) {
+    console.log(evt.target.id);
 }
 
 function render () {
 
+}
+
+function rotate () {
+    console.log("rotating!")
 }
 
 function hitOrMiss () {
